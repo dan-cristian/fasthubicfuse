9b56df1025497cf7464a2346206b07d9193955a0
dcristian <dan.cristian@gmail.com>
2015-11-08 23:13:15 +0200

    sfd

--- cloudfuse.c
+++ cloudfuse.c
@@ -666,23 +666,10 @@ int parse_option(void *data, const char *arg, int key, struct fuse_args *outargs
   return 1;
 }
 
-time_t my_timegm(struct tm *tm) {
-  time_t epoch = 0;
-  time_t offset = mktime(gmtime(&epoch));
-  time_t utc = mktime(tm);
-  return difftime(utc, offset);
-}
+
 
 int main(int argc, char **argv)
 {
-  time_t now = time(0);
-  struct tm local = *localtime(&now);
-  struct tm utc = *gmtime(&now);
-  time_t t1 = mktime(&local);
-  time_t t2 = my_timegm(&utc);
-  //assert(t1 == t2);
-  printf("t =%lu\nt1=%lu\nt2=%lu\n", now, t1, t2);
-
   char settings_filename[MAX_PATH_SIZE] = "";
   FILE *settings;
   struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
