0c441fbb9a7d464de315529ad6883e503e25a86a
dcristian <dan.cristian@gmail.com>
2015-11-11 01:45:51 +0200

    te

--- cloudfsapi.c
+++ cloudfsapi.c
@@ -1139,12 +1139,11 @@ int cloudfs_list_directory(const char *path, dir_entry **dir_list)
           }
           if (!strcasecmp((const char *)anode->name, "last_modified"))
           {
-            
             time_t last_modified_t = get_time_from_str_as_gmt(content);
             debugf("Got cloudfs_list_directory path=%s remote_time=%li.0 [%s]", de->name, last_modified_t, content);
             // utimens addition, set file change time on folder list, convert GMT time received from hubic as local
             char local_time_str[64];
-            time_t local_time_t = get_time_as_local(last_modified_t, local_time_str);
+            time_t local_time_t = get_time_as_local(last_modified_t, *local_time_str);
             debugf("Set cloudfs_list_directory path=%s local_time=%li.0 [%s]", de->name, local_time_t, local_time_str);
             de->last_modified = local_time_t;
             de->mtime.tv_sec = local_time_t;
