0995e4c3ac81b6d1d0ed37c50c57c921ea1841db
dcristian <dan.cristian@gmail.com>
2015-11-11 01:50:58 +0200

    ds

--- cloudfsapi.c
+++ cloudfsapi.c
@@ -105,7 +105,7 @@ time_t get_time_from_str_as_gmt(char *time_str){
   return val_time_t;
 }
 
-time_t get_time_as_local(time_t time_t_val, char *time_str){
+time_t get_time_as_local(time_t time_t_val, char time_str[]){
   struct tm loc_time_tm;
   loc_time_tm = *localtime(&time_t_val);
   if (time_str != NULL) {
