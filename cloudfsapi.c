bedc231f471a1e694cb58e8340781e3c7b96444c
dcristian <dan.cristian@gmail.com>
2015-11-11 02:43:13 +0200

    fsd

--- cloudfsapi.c
+++ cloudfsapi.c
@@ -283,10 +283,11 @@ static void header_set_time_from_str(char time_str[], int time_str_size, struct 
   time_t sec;
   long nsec;
   sscanf(time_str, "%[^.].%[^\n]", sec_value, nsec_value);
-  debugf("Received time=%li.%li, existing=%li.%li", sec_value, nsec_value, time_entry.tv_sec, time_entry.tv_nsec);
-  //strptime(sec_value, "%FT%T", &read_time);
   sec = atol(sec_value);
   nsec = atol(nsec_value);
+  debugf("Received time=%li.%li / %li.%li, existing=%li.%li", sec_value, nsec_value, sec, nsec, time_entry.tv_sec, time_entry.tv_nsec);
+  //strptime(sec_value, "%FT%T", &read_time);
+  
   if (sec != time_entry.tv_sec || nsec != time_entry.tv_nsec){
     debugf("Setting new time=%li.%li, existing was=%li.%li", sec, nsec, time_entry.tv_sec, time_entry.tv_nsec);
     time_entry.tv_sec = atol(sec_value);
