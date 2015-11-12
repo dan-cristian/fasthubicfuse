b7d205b256bc393c35b585c037fc1f32170915d4
dcristian <dan.cristian@gmail.com>
2015-11-12 22:40:43 +0200

    dfs

--- cloudfuse.c
+++ cloudfuse.c
@@ -756,6 +756,7 @@ void interrupt_handler(int sig) {
 
 int main(int argc, char **argv)
 {
+  debugf("Starting hubicfuse");
   signal(SIGINT, interrupt_handler);
 
   char settings_filename[MAX_PATH_SIZE] = "";
